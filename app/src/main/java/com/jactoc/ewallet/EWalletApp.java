9682783c80b80f48c44d87bdd31aa698cc8288cc
jactoc <jactoc@gmail.com>
2016-04-14 12:30:32 -0400

    sqlcipher new version, runtime permission for android 6.0 and new layout.

--- app/src/main/java/com/jactoc/ewallet/EWalletApp.java
+++ app/src/main/java/com/jactoc/ewallet/EWalletApp.java
@@ -1,12 +1,19 @@
 package com.jactoc.ewallet;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
 import com.jactoc.ewallet.database.QuerySource;
 import com.jactoc.ewallet.sharedPreferences.BasePreferences;
+import com.jactoc.ewallet.util.Util;
 
 import net.sqlcipher.database.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jactoc on 2016-03-19.
@@ -25,8 +32,13 @@ public class EWalletApp extends Application {
         //SQL
         SQLiteDatabase.loadLibs(getApplicationContext());
         dataSource = new QuerySource(getAppContext());
-        if(new BasePreferences(getAppContext()).getLoggedStatus() && new BasePreferences(getAppContext()).getPassword() != null)
-            dataSource.open(new BasePreferences(getAppContext()).getPassword());
+        if(new BasePreferences(getAppContext()).getLoggedStatus() && new BasePreferences(getAppContext()).getPassword() != null) {
+            if(Util.isAndroidM()) {
+                checkRuntimePermissions();
+            } else {
+                dataSource.open(new BasePreferences(getAppContext()).getPassword());
+            }
+        }
     }
 
     public static Context getAppContext() {
@@ -34,6 +46,22 @@ public class EWalletApp extends Application {
     }
     public static QuerySource getDataSource() {
         return dataSource;
+    }
+
+
+    private void checkRuntimePermissions() {
+        List<String> permissionsList = new ArrayList<>();
+        addPermission(permissionsList, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permissionsList.size() == 0) {
+            dataSource.open(new BasePreferences(getAppContext()).getPassword());
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.M)
+    private void addPermission(List<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+        }
     }
 
 } //end
